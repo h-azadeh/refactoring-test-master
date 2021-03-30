ec102c7ea8cfdf5dd7e35a596b7a15f73a729e80
Azadeh <az.homay@gmail.com>
2021-03-29 20:57:29 -0400

    UserService class AddUser method refactoring. also removed unused variable in UserCreditServiceClient class

--- LegacyApp/UserCreditService.cs
+++ LegacyApp/UserCreditService.cs
@@ -22,8 +22,7 @@ namespace LegacyApp
     [DebuggerStepThrough]
     [GeneratedCode("System.ServiceModel", "4.0.0.0")]
     public partial class UserCreditServiceClient : ClientBase<IUserCreditService>, IUserCreditService
-    {
-        private IUserCreditServiceChannel _userCreditServiceChannelImplementation;
+    {        
         public UserCreditServiceClient() {}
         
         public UserCreditServiceClient(string endpointConfigurationName) : 
--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -15,23 +15,15 @@ namespace LegacyApp
             {
                 return false;
             }
-
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
-            {
-                age--;
-            }
-
-            if (age < 21)
+                       
+            if (CalculateAge(dateOfBirth) < 21)
             {
                 return false;
             }
 
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
-
+            
             var user = new User
             {
                 Client = client,
@@ -41,32 +33,7 @@ namespace LegacyApp
                 Surname = surname
             };
 
-            if (client.Name == "VeryImportantClient")
-            {
-                // Skip credit chek
-                user.HasCreditLimit = false;
-            }
-            else if (client.Name == "ImportantClient")
-            {
-                // Do credit check and double credit limit
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.Firstname, user.Surname, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                // Do credit check
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.Firstname, user.Surname, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
+            SetCreditLimit(user, client);
 
             if (user.HasCreditLimit && user.CreditLimit < 500)
             {
@@ -76,6 +43,42 @@ namespace LegacyApp
             UserDataAccess.AddUser(user);
 
             return true;
+        }
+
+        private void SetCreditLimit(User user, Client client)
+        {
+            if (client.Name == "VeryImportantClient")
+            {
+                // Skip credit check
+                user.HasCreditLimit = false;
+            }
+            else
+            {
+                // Do credit check
+                user.HasCreditLimit = true;
+                using (var userCreditService = new UserCreditServiceClient())
+                {
+                    var creditLimit = userCreditService.GetCreditLimit(user.Firstname, user.Surname, user.DateOfBirth);
+
+                    if (client.Name == "ImportantClient")
+                        creditLimit = creditLimit * 2; //double credit limit
+
+                    user.CreditLimit = creditLimit;
+                }
+            }
+        }
+
+        private int CalculateAge(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
+            {
+                age--;
+            }
+
+            return age;
         }
     }
 }
